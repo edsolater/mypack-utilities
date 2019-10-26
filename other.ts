2a5733fcf13abe43a6e6add7e34af0a5ae89315f
edsol <edsolater@qq.com>
2019-10-26 20:23:37 +0800

    20191026 20:23

--- other.ts
+++
@@ -1,1 +0,0 @@
-const instantiate = constructorFunction => new constructorFunction()
--- string.js
+++ string.js
@@ -1,3 +1,5 @@
+import {intersect} from './toolKitsTRY'
+
 /**
  * 各种写法的互转：PascalCase\camelCase\
  * @example
@@ -8,3 +10,6 @@
  * toCase('snack_case', 'fooBar'); // 'foo_bar'
  */
 //TODO没写还
+
+[].pop()
+console.log(intersect([2,3],[4,5]))
