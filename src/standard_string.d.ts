3267c75e07abc0bd073e05d5fa68b3d3be975e9a
edsolater <edsolater@qq.com>
2019-10-30 14:44:38 +0800

    完全文件重命名

--- src/standard_string.d.ts
+++ src/standard_string.d.ts
@@ -1,4 +1,3 @@
-import {intersect} from './toolKitsTRY'
 
 /**
  * 各种写法的互转：PascalCase\camelCase\
@@ -10,6 +9,3 @@ import {intersect} from './toolKitsTRY'
  * toCase('snack_case', 'fooBar'); // 'foo_bar'
  */
 //TODO没写还
-
-[].pop()
-console.log(intersect([2,3],[4,5]))
